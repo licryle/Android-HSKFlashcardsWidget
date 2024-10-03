db71c1e6005f93abf5517aa37f2d161759d8c588
Licryle <cyrille@berliat.fr>
2024-10-03 22:41:03 +0800

    Moving the search bar to the top bar.

--- app/src/main/java/fr/berliat/hskwidget/MainActivity.kt
+++ app/src/main/java/fr/berliat/hskwidget/MainActivity.kt
@@ -1,7 +1,9 @@
 package fr.berliat.hskwidget
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,7 +12,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.navigation.fragment.NavHostFragment
 import fr.berliat.hskwidget.databinding.ActivityMainBinding
+import fr.berliat.hskwidget.ui.dictionary.DictionarySearchFragment
+import fr.berliat.hskwidget.ui.dictionary.DictionarySearchFragmentDirections
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +43,38 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setupSearchView()
+    }
+
+    private fun setupSearchView() {
+        val searchView: SearchView = findViewById(R.id.search_view)
+        // Set listener to handle search queries
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            // Triggered when the search button is pressed (or search query submitted)
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                hideKeyboard()
+
+                return onQueryTextChange(query)
+            }
+
+            // Triggered when the query text is changed
+            override fun onQueryTextChange(query: String?): Boolean {
+                val navHostFragment = supportFragmentManager.
+                findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+                val currentFragment = navHostFragment.childFragmentManager.fragments.firstOrNull()
+
+                if (currentFragment is DictionarySearchFragment) {
+                    currentFragment.performSearch(query.toString())
+                } else {
+                    val navController = findNavController(R.id.nav_host_fragment_content_main)
+                    val action = DictionarySearchFragmentDirections.search(query.toString())
+                    navController.navigate(action)
+                }
+
+                return true
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,5 +86,12 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun hideKeyboard() {
+        val imm = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        currentFocus?.let {
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 }
