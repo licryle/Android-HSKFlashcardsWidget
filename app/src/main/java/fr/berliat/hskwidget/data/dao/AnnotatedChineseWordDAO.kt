8291bed172c9b3272ede2a3c8970d5ccbc6e84b6
Licryle <cyrille@berliat.fr>
2024-10-05 22:44:44 +0800

    Now displaying the annotated words first, then by popularity.

--- app/src/main/java/fr/berliat/hskwidget/data/dao/AnnotatedChineseWordDAO.kt
+++ app/src/main/java/fr/berliat/hskwidget/data/dao/AnnotatedChineseWordDAO.kt
@@ -27,7 +27,8 @@ private const val select_left_join =
     "SELECT a.a_simplified, COALESCE(w.simplified, a.a_simplified) simplified, a.a_searchable_text, " +
             " a.a_pinyins, a.notes, a.class_type, a.class_level, a.themes, a.first_seen, a.is_exam," +
             " w.traditional, w.definition, w.hsk_level, w.pinyins, w.popularity, " +
-            " COALESCE(w.searchable_text, '') searchable_text " +
+            " COALESCE(w.searchable_text, '') searchable_text, " +
+            " (a.first_seen IS NULL) AS is_first_seen_null " +
             " FROM chinesewordannotation AS a LEFT JOIN chineseword AS w" +
             " ON a.a_simplified = w.simplified" +
             " "
@@ -36,26 +37,22 @@ private const val select_right_join =
     "SELECT COALESCE(a.a_simplified, w.simplified) a_simplified, w.simplified, " +
             " COALESCE(a.a_searchable_text, '') a_searchable_text, " +
             " a.a_pinyins, a.notes, a.class_type, a.class_level, a.themes, a.first_seen, a.is_exam," +
-            " w.traditional, w.definition, w.hsk_level, w.pinyins, w.popularity, w.searchable_text " +
+            " w.traditional, w.definition, w.hsk_level, w.pinyins, w.popularity, w.searchable_text, " +
+            " (a.first_seen IS NULL) AS is_first_seen_null " +
             " FROM chineseword AS w LEFT JOIN chinesewordannotation AS a" +
             " ON a.a_simplified = w.simplified" +
             " "
 
 @Dao
 interface AnnotatedChineseWordDAO {
- /**   @Query(select_outer_join)
-    suspend fun getAll(): List<AnnotatedChineseWord>
-
-    @Query("$select_outer_join WHERE w.searchable_text LIKE '%' || :str || '%' ORDER BY w.popularity DESC LIMIT :pageSize OFFSET (:page * :pageSize)")
-    suspend fun findWordFromStrLike(str: String?, page: Int = 0, pageSize: Int = 30): List<AnnotatedChineseWord>
-**/
      @Query("$select_left_join WHERE a.a_searchable_text LIKE '%' || :str || '%'" +
             " AND (0=:hasAnnotation OR (1=:hasAnnotation AND a.first_seen IS NOT NULL))" +
             " UNION " +
             "$select_right_join WHERE w.searchable_text LIKE '%' || :str || '%'" +
             " AND (0=:hasAnnotation OR (1=:hasAnnotation AND a.first_seen IS NOT NULL))" +
-            " ORDER BY w.popularity DESC LIMIT :pageSize OFFSET (:page * :pageSize)")
-     suspend fun _searchFromStrLike(str: String?, hasAnnotation: Boolean, page: Int = 0, pageSize: Int = 30): Map<ChineseWordAnnotation, List<ChineseWord>>
+            " ORDER BY is_first_seen_null, a.first_seen DESC, w.popularity DESC " +
+            " LIMIT :pageSize OFFSET (:page * :pageSize)")
+    suspend fun _searchFromStrLike(str: String?, hasAnnotation: Boolean, page: Int = 0, pageSize: Int = 30): Map<ChineseWordAnnotation, List<ChineseWord>>
 
     suspend fun searchFromStrLike(str: String, hasAnnotation: Boolean,
                                     page: Int = 0, pageSize: Int = 30
