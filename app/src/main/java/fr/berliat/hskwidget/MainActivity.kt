846da7b2028d3f92a1df1f588471c4ee1b4b01d8
licryle <cyrille@berliat.fr>
2025-06-12 03:02:53 +0800

    Minor cleanup of a variable that wasn't needed.

--- app/src/main/java/fr/berliat/hskwidget/MainActivity.kt
+++ app/src/main/java/fr/berliat/hskwidget/MainActivity.kt
@@ -295,7 +295,6 @@ class MainActivity : AppCompatActivity(), DatabaseBackupCallbacks {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
