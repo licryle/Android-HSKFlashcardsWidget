licryle/Android-HSKFlashcardsWidget

commit 815178721eb360a32588cb6ddd07b88af412ffde

Closing 2 memory leaks, one in filestream when copying backup files, and one in cursor when flushing database.

app/src/main/kotlin/fr/berliat/hskwidget/domain/Utils.kt
@@ -263,6 +263,9 @@ class Utils {
                 inputStream.copyTo(output)
             }
 
+            inputStream.close()
+            outFile.outputStream().close()
+
             return@withContext outFile
         }
 

crossPlatform/src/androidMain/kotlin/fr/berliat/hskwidget/data/dao/DatabaseManagementDAO.android.kt
@@ -4,6 +4,6 @@ import fr.berliat.hskwidget.data.store.ChineseWordsDatabase
 
 actual class DatabaseManagementDAO actual constructor(val db: ChineseWordsDatabase) {
     actual suspend fun flushDatabase() {
-        db.openHelper.writableDatabase.query("PRAGMA wal_checkpoint(FULL)")
+        db.openHelper.writableDatabase.query("PRAGMA wal_checkpoint(FULL)").close()
     }
 }

commit 4212098a881ee36160a71ad2b5c6bb271a380570

Fixing regression introduced by 815178721eb360a32588cb6ddd07b88af412ffde that closed an already closed outstream... Which led to a 0 length file.

app/src/main/kotlin/fr/berliat/hskwidget/domain/Utils.kt
@@ -142,7 +142,6 @@ class Utils {
             }
 
             inputStream.close()
-            outFile.outputStream().close()
 
             return@withContext outFile
         }