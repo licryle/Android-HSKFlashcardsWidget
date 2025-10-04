756a9f15137bc065d14c9155ec4dd5b42dfa39ae
licryle <cyrille@berliat.fr>
2025-10-05 05:03:35 +0800

    Add icons to menu items.

--- crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/application/drawer/AppDrawer.kt
+++ crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/application/drawer/AppDrawer.kt
@@ -3,6 +3,7 @@ package fr.berliat.hskwidget.ui.application.drawer
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import fr.berliat.hskwidget.ui.application.navigation.DecoratedScreen
 import fr.berliat.hskwidget.ui.application.navigation.Screen
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -32,8 +34,13 @@ fun AppDrawer(
         Spacer(Modifier.height(16.dp))
 
         menuItems.forEach { menuItem ->
+            val title = stringResource(menuItem.title)
             NavigationDrawerItem(
-                label = { Text(stringResource(menuItem.title)) },
+                label = { Text(title) },
+                icon = { Icon(
+                    painter = painterResource(menuItem.icon),
+                    contentDescription = title
+                ) },
                 selected = menuItem.screen::class == currentScreen::class,
                 onClick = { onNavigate(menuItem.screen) },
                 modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
