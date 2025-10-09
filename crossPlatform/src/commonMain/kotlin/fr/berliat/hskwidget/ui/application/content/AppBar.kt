e7d286e6b4901047d19307b028ce0f661790d81d
licryle <cyrille@berliat.fr>
2025-10-10 00:15:21 +0800

    Animate and style the App bar & Search box.

--- crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/application/content/AppBar.kt
+++ crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/application/content/AppBar.kt
@@ -1,7 +1,12 @@
 package fr.berliat.hskwidget.ui.application.content
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,18 +17,26 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
 
 import fr.berliat.hskwidget.Res
 import fr.berliat.hskwidget.close_24px
+import fr.berliat.hskwidget.menu
 import fr.berliat.hskwidget.menu_24px
 import fr.berliat.hskwidget.menu_ocr
 import fr.berliat.hskwidget.photo_camera_24px
+import fr.berliat.hskwidget.search_24px
+import fr.berliat.hskwidget.search_clear
 import fr.berliat.hskwidget.search_hint
 
 import org.jetbrains.compose.resources.painterResource
@@ -55,39 +68,71 @@ fun AppBar(
 
     TopAppBar(
         title = {
-            Row {
-                Text(title)
-
+            Row(verticalAlignment = Alignment.CenterVertically, // center children vertically
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.animateContentSize()
+            ) {
                 val focusRequester = remember { FocusRequester() }
                 val focusManager = LocalFocusManager.current
-                TextField(
-                    value = localText.value,
-                    onValueChange = { onValueChange(it) },
-                    placeholder = { Text(stringResource(Res.string.search_hint)) },
-                    modifier = Modifier.fillMaxWidth()
-                        .focusRequester(focusRequester),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            focusManager.clearFocus()
+                var isSearchFocused by remember { mutableStateOf(false) }
+
+                if (!isSearchFocused) {
+                    Text(title, modifier = Modifier.padding(end = 8.dp))
+
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+
+                Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.CenterEnd) {
+                    if (!isSearchFocused) {
+                        IconButton(onClick = { focusRequester.requestFocus(); isSearchFocused = true; }) {
+                            Icon(
+                                painterResource(Res.drawable.search_24px),
+                                contentDescription = stringResource(Res.string.search_hint)
+                            )
                         }
-                    ),
-                    singleLine = true,
-                    trailingIcon = {
-                        if (localText.value.isNotEmpty()) {
-                            IconButton(onClick = {
-                                focusRequester.requestFocus()
-                                onValueChange("")
-                            }) {
-                                Icon(painterResource(Res.drawable.close_24px), contentDescription = "Clear text")
+                    } else {
+                        LaunchedEffect(Unit) {
+                            focusRequester.requestFocus()
+                        }
+
+                        TextField(
+                            value = localText.value,
+                            onValueChange = { onValueChange(it) },
+                            placeholder = { Text(stringResource(Res.string.search_hint)) },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(focusRequester)
+                                .onFocusChanged { focusState ->
+                                    isSearchFocused = focusState.isFocused
+                                },
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    focusManager.clearFocus()
+                                }
+                            ),
+                            singleLine = true,
+                            trailingIcon = {
+                                if (localText.value.isNotEmpty()) {
+                                    IconButton(onClick = {
+                                        focusRequester.requestFocus()
+                                        onValueChange("")
+                                    }) {
+                                        Icon(
+                                            painterResource(Res.drawable.close_24px),
+                                            contentDescription = stringResource(Res.string.search_clear)
+                                        )
+                                    }
+                                }
                             }
-                        }
+                        )
                     }
-                )
+                }
             }
         },
         navigationIcon = {
             IconButton(onClick = onMenuClick) {
-                Icon(painterResource(Res.drawable.menu_24px), contentDescription = "Menu")
+                Icon(painterResource(Res.drawable.menu_24px),
+                    contentDescription = stringResource(Res.string.menu))
             }
         },
         actions = {
