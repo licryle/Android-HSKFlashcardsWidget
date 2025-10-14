7cc0f21a2b24b19e17598270db30caacaa347f44
licryle <cyrille@berliat.fr>
2025-10-15 01:44:18 +0800

    Making the ConfigScreen scrollable.

--- crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/screens/config/ConfigScreen.kt
+++ crossPlatform/src/commonMain/kotlin/fr/berliat/hskwidget/ui/screens/config/ConfigScreen.kt
@@ -1,9 +1,12 @@
 package fr.berliat.hskwidget.ui.screens.config
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
 import fr.berliat.hskwidget.core.HSKAppServices
 import fr.berliat.hskwidget.ui.components.AppDivider
 import fr.berliat.hskwidget.ui.screens.config.ankiSync.AnkiSyncView
@@ -19,7 +22,11 @@ fun ConfigScreen(
         gDriveBackup = HSKAppServices.gDriveBackup
     ) }
 ) {
-    Column(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+    val scrollState = rememberScrollState()
+    Column(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .verticalScroll(scrollState)) {
         BackupDiskView(modifier, viewModel = viewModel.backupDiskViewModel)
 
         AppDivider()
