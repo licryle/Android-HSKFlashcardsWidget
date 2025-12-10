710cfbd618e73209c84adc6339ae128f6b9c04b2
Licryle <licryle@users.noreply.github.com>
2025-12-10 20:17:06 +0800

    iOS: Connect WidgetList speakWord to Utils .playWordInBackground

--- crossPlatform/src/iosMain/kotlin/fr/berliat/hskwidget/ui/screens/widget/WidgetsListViewModel.ios.kt
+++ crossPlatform/src/iosMain/kotlin/fr/berliat/hskwidget/ui/screens/widget/WidgetsListViewModel.ios.kt
@@ -2,11 +2,13 @@ package fr.berliat.hskwidget.ui.screens.widget
 
 import kotlinx.coroutines.flow.StateFlow
 
+import fr.berliat.hskwidget.core.Utils
+
 actual class WidgetsListViewModel actual constructor() {
     actual val widgetIds: StateFlow<List<Int>>
         get() = TODO("Not yet implemented")
 
     actual fun addNewWidget() { }
 
-    actual fun speakWord(word: String) { }
+    actual fun speakWord(word: String) = Utils.playWordInBackground(word)
 }
