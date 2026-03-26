4d14554c81c1b0a20c4900802bb491d344833b01
licryle <cyrille@berliat.fr>
2026-03-26 19:33:46 +0800

    Add firebase to pod for crashalytics, and for GoogleDriveBackup Support. Update GoogleDriveBackup to head (v2.0.0), to enable iOS support.

--- iosApp/hskwidget/hskwidgetApp.swift
+++ iosApp/hskwidget/hskwidgetApp.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
 import crossPlatform
 
 struct ComposeView: UIViewControllerRepresentable {
@@ -10,8 +12,24 @@ struct ComposeView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    
+    // Attempt to get the Client ID from Firebase options first, then fallback to Info.plist
+    if let clientID = FirebaseApp.app()?.options.clientID ?? Bundle.main.object(forInfoDictionaryKey: "GIDClientID") as? String {
+        GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientID)
+    }
+
+    return true
+  }
+}
+
 @main
 struct hskwidgetApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ComposeView()
